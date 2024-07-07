f745f93f25c1d030759b70fcf33a4dc85594d93b
Tanish Mangal <tanishmangal615@gmail.com>
2024-07-07 16:19:58 +0530

    first commit

---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+    appwriteURl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteprojectId : String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf
